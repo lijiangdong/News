d1000d30b3e5aaec0483f58b5e214870091f7abc
lijiangdong <ljd2038@gmail.com>
2016-07-15 16:48:19 +0800

    commit

--- app/src/main/java/com/ljd/news/presentation/view/activity/MainActivity.java
+++ app/src/main/java/com/ljd/news/presentation/view/activity/MainActivity.java
@@ -45,6 +45,8 @@ import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 
 public class MainActivity extends BaseActivity implements HasComponent<ZhiHuComponent>,
         BaseFragment.HandleFloatActionButton,CheckNewsUpdateView{
@@ -107,6 +109,7 @@ public class MainActivity extends BaseActivity implements HasComponent<ZhiHuComp
                 case R.id.nav_home:
                     break;
                 case R.id.nav_share:
+                    this.showShare();
                     break;
             }
             drawerLayout.closeDrawers();
@@ -157,7 +160,6 @@ public class MainActivity extends BaseActivity implements HasComponent<ZhiHuComp
         this.fab.showMenu(true);
     }
 
-
     @Override
     public void updatePromptView(String versionName, String message) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -171,5 +173,34 @@ public class MainActivity extends BaseActivity implements HasComponent<ZhiHuComp
             dialog.dismiss();
         });
         builder.create().show();
+    }
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle("share");
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
     }
 }
